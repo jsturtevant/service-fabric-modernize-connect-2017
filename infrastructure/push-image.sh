683de3f85dd470a97cc69cfaa12b83339610d8f9
James Sturtevant <jsturtevant@gmail.com>
2017-11-16 09:55:15 -0800

    clean up

--- infrastructure/push-image.sh
+++ infrastructure/push-image.sh
@@ -3,6 +3,6 @@ image=$1
 version=$2
 
 
-docker login -u sfconnect2017 -p iNbWQw==/81yJNOqqtgoSR0qIPeMdXGT sfconnect2017.azurecr.io
+docker login -u sfconnect2017 -p  sfconnect2017.azurecr.io
 docker tag $image:latest sfconnect2017.azurecr.io/$image:$version
 docker push sfconnect2017.azurecr.io/$image:$version
